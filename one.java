697992332392504d9f768bca5358d32c925fe44b
sowmiyaM-1 <sowmekyle@gmail.com>
2025-01-16 18:54:28 +0530

    feat: added the program

--- one.java
+++ one.java
@@ -1,1 +1,24 @@
 import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
